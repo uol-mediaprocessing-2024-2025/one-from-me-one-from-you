72e201dabe249dd28dbf98afa01bc8d2b0538b45
Oliver Kuhn <oliver.kuhn@uni-oldenburg.de>
2024-11-15 20:25:03 +0100

    added age api fetch

---
+++ backend/sortByAge.py
@@ -0,0 +1,11 @@
+import requests
+
+client_id = 'replace'
+client_secret = 'this'
+params = {'url': 'https://labs.everypixel.com/static/i/estest_sample3.jpg'}
+quality = requests.get('https://api.everypixel.com/v1/faces', params=params, auth=(client_id, client_secret)).json()
+
+with open('image.jpg','rb') as image:
+    data = {'data': image}
+    quality = requests.post('https://api.everypixel.com/v1/faces', files=data, auth=(client_id, client_secret)).json()
+    print(quality)
